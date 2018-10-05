88c09908d7cacf1684f2a0de105bde21a63245c7
Debora Dian Pratama <deboradian09@gmail.com>
2018-10-05 21:01:52 +0700

    commit 1

---
+++ src/main/java/com/apap/tutorial4/service/FlightService.java
@@ -0,0 +1,12 @@
+package com.apap.tutorial4.service;
+
+import com.apap.tutorial4.model.FlightModel;
+
+/**
+ * FlightService
+ * @author debora
+ */
+
+public interface FlightService {
+	void addFlight(FlightModel flight);
+}
---
+++ src/main/java/com/apap/tutorial4/service/PilotServiceImpl.java
@@ -0,0 +1,32 @@
+package com.apap.tutorial4.service;
+
+import com.apap.tutorial4.model.PilotModel;
+import com.apap.tutorial4.repository.PilotDb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * PilotServiceImpl
+ * @author debora
+ */
+@Service
+@Transactional
+public class PilotServiceImpl implements PilotService{
+	@Autowired
+	private PilotDb pilotDb;
+	
+	@Override
+	public PilotModel getPilotDetailByLicenseNumber(String licenseNumber) {
+		return pilotDb.findByLicenseNumber(licenseNumber);
+	}
+
+	@Override
+	public void addPilot(PilotModel pilot) {
+		pilotDb.save(pilot);
+	}
+	
+	
+
+}
